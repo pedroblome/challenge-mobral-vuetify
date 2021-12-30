564be74911cc8c15e9458d6a87eb49027a355529
pedroblome <joaopedroblome78@gmail.com>
2021-12-30 11:31:36 -0300

    adding JoaoPedro view

--- vuetify-mobral/src/main.js
+++ vuetify-mobral/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 
 new Vue({
